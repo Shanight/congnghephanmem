1d143a022510044ce962e4bf04a80651bdbf73fb
Phan Gia Huy <phangiahuy8033@gmail.com>
2024-03-27 08:31:36 +0700

    up

--- BaiLam/test3/test3/obj/Debug/net8.0/test3.AssemblyInfo.cs
+++ BaiLam/test3/test3/obj/Debug/net8.0/test3.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ad97bb01a08e44016f5df4e606d8e6d03f42dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36ecca2d64034024674e0dc7528668776408998c")]
 [assembly: System.Reflection.AssemblyProductAttribute("test3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
